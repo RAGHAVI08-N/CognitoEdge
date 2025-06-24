f61193369bfa06d9babb20fe0a12fa5b33042c5a
Pavithra2148 <pavics217@rmkcet.ac.in>
2025-06-24 15:25:49 +0530

    login added

--- src/app/Modules/auth/login/login.component.ts
+++ src/app/Modules/auth/login/login.component.ts
@@ -1,11 +1,50 @@
+
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports:[SharedModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private message : NzMessageService,
+    private router: Router,
+  ) {}
+
+  validateForm!: FormGroup;
+
+  ngOnInit() {
+    this.validateForm=this.fb.group({
+      email: [null, Validators.required],
+      password: [null, Validators.required]
+    })
+  }
+  submitForm(){
+    this.authService.login(this.validateForm.value).subscribe(res=>{
+      this.message
+      .success(
+        `Login Success.`,
+        { nzDuration: 5000}
+      );
+      console.log(res);
+    },  error=>{
+      this.message
+      .error(
+        `Bad credentials`,
+        { nzDuration: 5000}
+      );
+    }
+    )
+  }
 
 }
--- src/app/Modules/auth/signup/signup.component.ts
+++ src/app/Modules/auth/signup/signup.component.ts
@@ -1,48 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { HttpClientModule } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports:[SharedModule,HttpClientModule],
+  imports: [SharedModule],
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
-  constructor (private fb : FormBuilder,
+export class SignupComponent implements OnInit {
+  validateForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
     private message: NzMessageService,
     private router: Router,
     private authService: AuthService
+  ) {}
 
-  ){}
-
-  validateForm!: FormGroup;
-  ngOnInit(){
+  ngOnInit() {
     this.validateForm = this.fb.group({
+      name: [null, [Validators.required]],
       email: [null, [Validators.required, Validators.email]],
-      password: [null,[Validators.required]],
-      name:[null,[Validators.required]]
-
-    })
+      password: [null, [Validators.required]]
+    });
   }
 
   submitForm() {
-  this.authService.register(this.validateForm.value).subscribe(
-    res => {
-      this.message.success('Signup successful', { nzDuration: 5000 });
-      this.router.navigateByUrl('/login');
-    },
-    error => {
-      this.message.error(`${error.error}`, { nzDuration: 5000 });
-    }
-  );
-}
-
-
+    this.authService.register(this.validateForm.value).subscribe(
+      res => {
+        this.message.success('Signup successful', { nzDuration: 5000 });
+        this.router.navigateByUrl('/login');
+      },
+      error => {
+        const errorMessage = error?.error || 'User already exists or signup failed';
+        this.message.error(errorMessage, { nzDuration: 5000 });
+      }
+    );
+  }
 }
