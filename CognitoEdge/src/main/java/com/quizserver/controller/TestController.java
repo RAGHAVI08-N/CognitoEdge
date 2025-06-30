f7e0620a64ab0a3be36a4f2c17bed525d3c72a86
Pavithra2148 <pavics217@rmkcet.ac.in>
2025-06-30 11:51:09 +0530

    init commit

--- CognitoEdge/src/main/java/com/quizserver/controller/TestController.java
+++ CognitoEdge/src/main/java/com/quizserver/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.quizserver.controller;
 
 
+import com.quizserver.dto.QuestionDTO;
 import com.quizserver.dto.TestDTO;
 import com.quizserver.service.test.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,4 +24,29 @@ public class TestController {
              return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
          }
      }
+
+     @PostMapping("/question")
+     public ResponseEntity<?> addQuestionInTest(@RequestBody QuestionDTO dto){
+         try{
+             return new ResponseEntity<>(testService.addQuestionInTest(dto), HttpStatus.CREATED);
+         } catch(Exception e){
+             return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+         }
+     }
+     @GetMapping()
+     public ResponseEntity<?> getAllTest(){
+         try{
+             return new ResponseEntity<>(testService.getAllTests(), HttpStatus.OK);
+         } catch (Exception e){
+             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+         }
+     }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAllQuestions(@PathVariable Long id){
+        try{
+            return new ResponseEntity<>(testService.getAllQuestionByTest(id), HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- CognitoEdge/src/main/java/com/quizserver/service/test/TestService.java
+++ CognitoEdge/src/main/java/com/quizserver/service/test/TestService.java
@@ -1,8 +1,15 @@
 package com.quizserver.service.test;
 
-import com.quizserver.dto.TestDTO;
+import com.quizserver.dto.*;
+import com.quizserver.entities.Test;
+
+import java.util.List;
 
 public interface TestService {
 
     TestDTO createTest(TestDTO dto);
+    QuestionDTO addQuestionInTest(QuestionDTO dto);
+    List<TestDTO> getAllTests();
+    TestDetailsDTO getAllQuestionByTest(Long id);
+    TestResultDTO submitTest(SubmitTestDTO request);
 }
