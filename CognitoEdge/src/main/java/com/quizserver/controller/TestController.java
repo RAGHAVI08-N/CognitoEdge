16f5cb527dbefa7b388543be26e0f442cdb328f6
Pavithra2148 <pavics217@rmkcet.ac.in>
2025-06-30 11:51:09 +0530

    init commit

--- CognitoEdge/src/main/java/com/quizserver/controller/TestController.java
+++ CognitoEdge/src/main/java/com/quizserver/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.quizserver.controller;
 
 
+import com.quizserver.dto.QuestionDTO;
 import com.quizserver.dto.TestDTO;
 import com.quizserver.service.test.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,23 @@ public class TestController {
      public ResponseEntity<?> createTest(@RequestBody TestDTO dto){
          try{
              return new ResponseEntity<>(testService.createTest(dto), HttpStatus.OK);
+         } catch (Exception e){
+             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+         }
+     }
+
+     @PostMapping("/question")
+     public ResponseEntity<?> addQuestionInTest(@RequestBody QuestionDTO dto){
+         try{
+             return new ResponseEntity<>(testService.addQuestionInTest(dto), HttpStatus.CREATED);
+         } catch(Exception e){
+             return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+         }
+     }
+     @GetMapping()
+     public ResponseEntity<?> getAllTest(){
+         try{
+             return new ResponseEntity<>(testService.getAllTests(), HttpStatus.OK);
          } catch (Exception e){
              return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
          }
